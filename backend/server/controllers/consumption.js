cad2ffefc282c7c7bee3bd312259634b8a5d070f
Hobar <huparelaa@eafit.edu.co>
2023-07-16 18:40:18 -0500

    API consumption history added

--- backend/server/controllers/consumption.js
+++ backend/server/controllers/consumption.js
@@ -15,3 +15,16 @@ export async function addComsumption(req, res) {
     return res.status(500).json({ message: error.message });
   }
 }
+
+export async function viewConsumptionHistory(req,res){
+  try {
+    const user_id = req.params.user_id
+    const [result] = await pool.query("SELECT * FROM `Consumption` WHERE user_id = ?",[user_id])
+    return res.json({
+      message:"Success",
+      result
+    })
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
--- backend/server/routes/index.routes.js
+++ backend/server/routes/index.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { signup, login } from "../controllers/auth.controllers.js";
 import { pool } from "../db.js";
-import { addComsumption } from "../controllers/consumption.js";
+import { addComsumption, viewConsumptionHistory } from "../controllers/consumption.js";
 import { getUserInfo } from "../controllers/userInfo.js";
 
 const router = Router();
@@ -10,10 +10,12 @@ router.get("/test", async (req, res) => {
   const [result] = await pool.query("SELECT * FROM `User`");
   return res.json(result);
 });
-
+//User
 router.get("/getUserInfo/:user_id", getUserInfo)
-
 router.post("/signup", signup);
 router.post("/login", login);
+
+//Consumption
 router.post("/consumption/add", addComsumption);
+router.get("/consumption/view/:user_id", viewConsumptionHistory)
 export default router;
